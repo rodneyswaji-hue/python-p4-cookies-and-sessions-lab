ab7e28850eaa4edc3ece605bf5d3631fe6f54a7c
Rodney Swaji <rodney.swaji@student.moringaschool.com>
2026-01-13 18:21:23 +0300

    intial commit

--- server/app.py
+++ server/app.py
@@ -22,13 +22,29 @@ def clear_session():
 
 @app.route('/articles')
 def index_articles():
-
-    pass
+     articles = Article.query.all()
+     return jsonify([article.to_dict() for article in articles]), 200
 
 @app.route('/articles/<int:id>')
 def show_article(id):
+    # Initialize page_views if it doesn't exist
+    session['page_views'] = session.get('page_views', 0)
 
-    pass
+    # Increment page_views
+    session['page_views'] += 1
+
+    # Fetch the article
+    article = Article.query.filter_by(id=id).first()
+
+    if not article:
+        return jsonify({"message": "Article not found"}), 404
+
+    # Allow up to 3 page views
+    if session['page_views'] <= 3:
+        return jsonify(article.to_dict()), 200
+    else:
+        return jsonify({"message": "Maximum pageview limit reached"}), 401
+
 
 if __name__ == '__main__':
     app.run(port=5555)
